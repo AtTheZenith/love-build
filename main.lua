196af14b1208274e57394da93d212bdc86c2f8a4
zenith <atthezenith@proton.me>
2025-12-24 15:09:18 +0300

    replace with slick

--- main.lua
+++ main.lua
@@ -1,64 +1,98 @@
-local camera = require 'src.camera'
-local input = require 'src.input'
-
-local currentcamera
-local randomobjects = {}
-
-local sin, cos = {}, {}
-for i = 0, 360 do
-  local radians = math.rad(i)
-  sin[i] = math.sin(radians)
-  cos[i] = math.cos(radians)
+local jit = require("jit")
+if jit.arch == "arm64" and love.system.getOS() == "OS X" then
+    jit.off()
 end
 
 
-function love.load()
-  love.window.setTitle 'Hello World'
-  love.window.setMode(800, 600, { resizable=true })
+local bill = require("demo.bill")
+local lonk = require("demo.lonk")
+local luigini = require("demo.luigini")
+local mush = require("demo.mush")
 
-  ---@cast currentcamera camera
-  currentcamera = camera:new()
-  for _ = 1, 10 do
-    table.insert(randomobjects, {
-      x = math.random(-400, 400),
-      y = math.random(-300, 300),
-      size = math.random(20, 50)
-    })
-  end
+local currentDemo = lonk
+local showHelp = false
 
-  print 'Hello, World!'
+function love.keypressed(key, scan, isRepeat)
+    local handled = false
+    if not isRepeat then
+        if key == "1" then
+            currentDemo = lonk
+            handled = true
+        elseif key == "2" then
+            currentDemo = luigini
+            handled = true
+        elseif key == "3" then
+            currentDemo = bill
+            handled = true
+        elseif key == "4" then
+            currentDemo = mush
+            handled = true
+        elseif key == "f1" then
+            showHelp = not showHelp
+        end
+    end
+
+    if not handled and currentDemo.keypressed then
+        currentDemo.keypressed(key, scan, isRepeat)
+    end
 end
 
-function love.update(dt)
-  local movespeed = 200
-  local zoomspeed = 0.5
-
-  if input.isdown("a") then
-    currentcamera.x = currentcamera.x + movespeed * dt
-  end
-  if input.isdown("d") then
-    currentcamera.x = currentcamera.x - movespeed * dt
-  end
-  if input.isdown("w") then
-    currentcamera.y = currentcamera.y + movespeed * dt
-  end
-  if input.isdown("s") then
-    currentcamera.y = currentcamera.y - movespeed * dt
-  end
+function love.keyreleased(...)
+    if currentDemo.keyreleased then
+        currentDemo.keyreleased(...)
+    end
 end
+
+function love.mousepressed(...)
+    if currentDemo.mousepressed then
+        currentDemo.mousepressed(...)
+    end
+end
+
+function love.mousemoved(...)
+    if currentDemo.mousemoved then
+        currentDemo.mousemoved(...)
+    end
+end
+
+function love.update(deltaTime)
+    if not showHelp then
+        currentDemo.update(deltaTime)
+    end
+end
+
+local help = [[
+slick demo
+
+- press f1 to close help
+
+global controls
+- 1: legend of lönk top-down "RPG" demo
+- 2: luigini brothers platformer demo
+- 3: bill c. triangulation demo
+- 4: mush navigation demo
+]]
 
 function love.draw()
-  currentcamera:refreshcanvas()
-  currentcamera:enable()
-  for _, obj in next, randomobjects do
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.rectangle("line", obj.x - obj.size/2, obj.y - obj.size/2, obj.size, obj.size)
-  end
-  love.graphics.setColor(1, 0, 0)
-  love.graphics.circle("fill", 0, 0, 10)
-  currentcamera:disable()
+    love.graphics.push("all")
+    if showHelp then
+        love.graphics.print(help, 8, 8)
+        
+        local _, lines = love.graphics.getFont():getWrap(help, love.graphics.getWidth())
+        love.graphics.translate(0, (#lines + 1) * love.graphics.getFont():getHeight())
 
-  love.graphics.circle("fill", love.graphics.getWidth()/2, love.graphics.getHeight()/2, 15)
+        if currentDemo.help then
+            currentDemo.help()
+        end
+    else
+        love.graphics.push("all")
+        currentDemo.draw()
+        love.graphics.pop()
 
-  love.graphics.printf('Hello World!', 0, 20, love.graphics.getWidth(), 'center')
+        love.graphics.setColor(0, 0, 0, 1)
+        love.graphics.printf(("press f1 for help (%d FPS)"):format(love.timer.getFPS()), 1, 9, love.graphics.getWidth(), "center")
+        love.graphics.setColor(1, 0, 1, 1)
+        love.graphics.printf(("press f1 for help (%d FPS)"):format(love.timer.getFPS()), 0, 8, love.graphics.getWidth(), "center")
+    end
+    love.graphics.pop()
 end
